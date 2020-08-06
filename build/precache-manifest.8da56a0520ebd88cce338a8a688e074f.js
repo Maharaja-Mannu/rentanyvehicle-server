e4ef8417980de854fb22d1c1bb7b9942363ed9e0
Mannu Tanti <maharajamannu@gmail.com>
2020-08-06 09:10:06 +0530

    production build folder added

---
+++ build/precache-manifest.8da56a0520ebd88cce338a8a688e074f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d93330770c18c616e3b1deca3c6bf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d521a1bbecdbed2f9cc",
+    "url": "/static/css/main.d959b553.chunk.css"
+  },
+  {
+    "revision": "720402a4ebe5d8785362",
+    "url": "/static/js/2.0989db40.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.0989db40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d521a1bbecdbed2f9cc",
+    "url": "/static/js/main.1871d885.chunk.js"
+  },
+  {
+    "revision": "5a21c3cd6ed3f10a78a5",
+    "url": "/static/js/runtime-main.d3a30d8b.js"
+  }
+]);
