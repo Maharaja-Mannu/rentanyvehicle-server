e192997b2bf0d56f750dbf7c488f2303128e0151
Mannu Tanti <maharajamannu@gmail.com>
2020-08-29 16:40:31 +0530

    Changed get request to post

--- src/routers/vehicle.js
+++ src/routers/vehicle.js
@@ -5,28 +5,38 @@ const Vehicle = require('../models/vehicle')
 const auth = require('../middleware/auth')
 const router = express.Router()
 
-router.post('/vehicle', async (req, res) => {
-    const data = await Vehicle.find({ origin: req.body.origin})
-    res.send({ data })
-})
-
-router.get('/vehicle/:id', async (req, res) => {
+router.get('/vehicle/:category/:origin/:dest', async (req, res) => {
+    let query
+    // for(param in req.params) {
+    //     console.log(req.params[param])
+    // }
+    query = {$and: [{category: req.params.category}, {origin: req.params.origin}]}
     try {
-        const data = await Vehicle.findOne({ _id: req.params.id })
-        if (!data) {
-            return res.status(400).send()
-        }
-        res.status(200).send({ data })
+        const data = await Vehicle.find(query)
+        if (!data) return res.status(400).send("Invalid request!")
+        res.send({ data })
     } catch (error) {
         res.status(500).send(error.message)
     }
 })
 
-router.get('/user/ads', auth, async (req, res) => {
+router.post('/vehicle/:id', async (req, res) => {
+    try {
+        const data = await Vehicle.findOne({ _id: req.params.id })
+        if (!data) {
+            return res.status(400).send("Invalid request!")
+        }
+        res.status(200).send({ data })
+    } catch (error) {
+        res.status(400).send("Bad Request")
+    }
+})
+
+router.post('/user/ads', auth, async (req, res) => {
     try {
         const data = await Vehicle.find({ owner: req.user._id })
         if (!data) {
-            return res.status(400).send()
+            return res.status(400).send("Invalid request!")
         }
         res.send({ data })
     } catch (error) {
@@ -58,7 +68,7 @@ router.post('/submitad', auth, upload.single('images'), async (req, res, next) =
         await vehicle.save()
         res.status(201).send(vehicle)
     } catch (error) {
-        res.status(400).send(error.message)
+        res.status(500).send(error.message)
     }
 })
 
@@ -69,7 +79,7 @@ router.patch('/vehicle/:id', auth, async (req, res) => {
     delete req.body.createdAt
     delete req.body.updatedAt
     const updates = Object.keys(req.body)
-    const allowedUpdates = ['_id', 'brand', 'category', 'images', 'model', 'origin', 'reg_number', 'owner']
+    const allowedUpdates = ['brand', 'category', 'images', 'model', 'origin', 'reg_number', 'owner']
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
     if (!isValidOperation) {
         return res.status(400).send('Invalid update')
@@ -79,13 +89,13 @@ router.patch('/vehicle/:id', auth, async (req, res) => {
             _id: req.params.id, owner: req.user._id
         })
         if (!data) {
-            return res.status(400).send()
+            return res.status(400).send("Invalid request!")
         }
         updates.forEach(update => data[update] = req.body[update])
         await data.save()
-        res.send("Updated Successfully!")
+        res.send({ data })
     } catch (error) {
-        res.status(400).send(error.message)
+        res.status(500).send(error.message)
     }
 })
 
@@ -94,7 +104,7 @@ router.delete('/vehicle/:id', auth, async (req, res) => {
     try {
         const data = await Vehicle.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if (!data) {
-            return res.status(404).send()
+            return res.status(400).send("Invalid request!")
         }
         res.send()
     } catch (error) {
